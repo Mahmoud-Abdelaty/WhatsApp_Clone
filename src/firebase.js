c36ae28a4c01267232cf52c729da9e68312cbee2
Mahmoud Abdelaty <126201317+Mahmoud-Abdelaty@users.noreply.github.com>
2024-02-06 22:00:03 +0200

    Update Firebase
    Update Firebase File to Solve Security Issues

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import "firebase/compat/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBTh-KWyEKvAME-4ppVvlmCar70aXJS2No",
-  authDomain: "whatsapp-clone-2ca38.firebaseapp.com",
-  projectId: "whatsapp-clone-2ca38",
-  storageBucket: "whatsapp-clone-2ca38.appspot.com",
-  messagingSenderId: "327706171265",
-  appId: "1:327706171265:web:e71bda6d35fcdd7272ba80",
-  measurementId: "G-XZ4PR7X7R1",
+  apiKey: "AIzaSyBtrHO6pKhgvYlcZBOPa8AsCkSa8xC_zyk",
+  authDomain: "whatsapp-clone-8f994.firebaseapp.com",
+  projectId: "whatsapp-clone-8f994",
+  storageBucket: "whatsapp-clone-8f994.appspot.com",
+  messagingSenderId: "1086482323936",
+  appId: "1:1086482323936:web:780dc5bf43a2f30f20b385",
+  measurementId: "G-6S8PJP95VJ",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
